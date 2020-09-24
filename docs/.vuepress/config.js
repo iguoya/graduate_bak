f813ead0f8297e9ff4378937d1813f0065d6fc18
iguoya <375478250@qq.com>
2020-09-24 17:57:24 +0800

    fix title

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,42 +1,10 @@
 module.exports = {
-  title: ' 设计模式',
-  description: ' 李建忠---可复用的软件设计',
+  title: ' English Notebook',
+  description: ' Practice and Writing ',
   base: '/English/',
 //  dest: 'public',
   plugins: [
     ['autobar'],
-    [
-      'vuepress-plugin-mathjax',
-      {
-        target: 'svg',
-        macros: {
-          '*': '\\times',
-        },
-      },
-    ],
-    [
-      'vuepress-plugin-mermaidjs'
-    ],
-    [
-      'vuepress-plugin-medium-zoom',
-      {
-        selector: '.my-wrapper .my-img',
-        delay: 1000,
-        options: {
-          margin: 24,
-          background: '#BADA55',
-          scrollOffset: 0,
-        },
-      },
-    ],
-    [
-      'vuepress-plugin-typescript',
-      {
-        tsLoaderOptions: {
-          // ts-loader 的所有配置项
-        },
-      },
-    ],
   ],
   themeConfig: {
     nav: [
