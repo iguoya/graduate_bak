991d69b179a56a3321c80db172e349e52433f250
iguoya <375478250@qq.com>
2020-09-24 11:29:29 +0800

    测试自动构建

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  title: ' 英语笔记 ',
+  description: '  阅读和写作',
+  base: '/English/',
+//  dest: 'public',
+  plugins: [
+    ['autobar'],
+    [
+      'vuepress-plugin-mermaidjs'
+    ],
+    [
+      'vuepress-plugin-medium-zoom',
+      {
+        selector: '.my-wrapper .my-img',
+        delay: 1000,
+        options: {
+          margin: 24,
+          background: '#BADA55',
+          scrollOffset: 0,
+        },
+      },
+    ],
+    [
+      'vuepress-plugin-typescript',
+      {
+        tsLoaderOptions: {
+          // ts-loader 的所有配置项
+        },
+      },
+    ],
+  ],
+  themeConfig: {
+    nav: [
+      {
+        text: '首页',
+        link: '/home/'
+      },
+      {
+        text: 'H5',
+        link: '/H5/'
+      },
+      {
+        text: '规范',
+        link: '/Standard/'
+      }
+    ],
+    sidebar: [
+
+    ]
+  }
+}
